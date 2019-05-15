8a74d163ee622b4c7b8712727b4db24f834b0765
George <piatskgeorge@mail.ru>
2019-05-16 01:41:32 +0300

    Create list.cpp

---
+++ list.cpp
@@ -0,0 +1,56 @@
+#include "node.h"
+
+template <typename T>
+class LinkedList
+{
+  Node<T> *m_begin;
+
+
+  int m_size = 0;
+public:
+  LinkedList ()
+  {
+  }
+  int size ()
+  {
+  }
+  
+  /*Inserts node with input value after specified position*/
+  Node<T> *insert (T value, int pos) 
+  {
+  }
+
+  /*Inserts node with input value in the end of the list */
+  Node<T> *push_back (T value)
+  {
+  }
+  
+  /*Finds first node from the begin with given value*/
+  Node<T> *find (T value)
+  {
+  }
+  
+  /*removes input node*/
+  void remove (Node<T> *node)
+  {
+  }
+
+  /*Reverses the list*/
+  void reverse ()
+  {
+  }
+  
+  /*Clears the list*/
+  void clear ()
+  {
+  }
+  
+  /*Prints list to stdout*/
+  void print_list ()
+  {
+  }
+
+  ~LinkedList ()
+  {
+  }
+};
